f57cb869d70cf1a3404bc484b22448590610d8c2
Natsumi.H <natsmy.1211@gmail.com>
2024-05-25 17:21:33 +0800

    update profile

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,63 +1,63 @@
 import "@/pages/Performance/Performance.css";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { Box, Button, Flex, Image, Table, Title } from "@mantine/core";
-import { getToken } from "../../util/security";
-import { Link, useNavigate, useOutletContext } from "react-router-dom";
+// import { getToken } from "../../util/security";
+import { Link } from "react-router-dom";
 import mv from "@/assets/mv.png";
 import styles from "@/components/Parts/Button/Button.module.scss";
 
 function Profile() {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [scoreId, setScoreId] = useState(null);
-  const { user } = useOutletContext();
-  const navigate = useNavigate();
+  // const [data, setData] = useState(null);
+  // const [loading, setLoading] = useState(true);
+  // const [scoreId, setScoreId] = useState(null);
+  // const { user } = useOutletContext();
+  // const navigate = useNavigate();
 
-  useEffect(() => {
-    if (!user) {
-      navigate("/");
-      return;
-    }
-    const getData = async () => {
-      const options = {
-        method: "GET",
-      };
-      const token = getToken();
-      if (token) {
-        options.headers = options.headers || {};
-        options.headers.Authorization = `Bearer ${token}`;
-      }
+  // useEffect(() => {
+  //   if (!user) {
+  //     navigate("/");
+  //     return;
+  //   }
+  //   const getData = async () => {
+  //     const options = {
+  //       method: "GET",
+  //     };
+  //     const token = getToken();
+  //     if (token) {
+  //       options.headers = options.headers || {};
+  //       options.headers.Authorization = `Bearer ${token}`;
+  //     }
 
-      const res = await fetch(
-        `${import.meta.env.VITE_API_BASEURL}/score/`,
-        options
-      );
-      const json = await res.json();
-      const firstOne = json[0];
-      setScoreId(firstOne._id);
-      setData([
-        {
-          skill: "Communication Skill",
-          points: firstOne.communication,
-        },
-        {
-          skill: "Creativity",
-          points: firstOne.creativity,
-        },
-        {
-          skill: "Empathy",
-          points: firstOne.empathy,
-        },
-        {
-          skill: "Problem-Solving Skill",
-          points: firstOne.problemsolving,
-        },
-      ]);
-      setLoading(false);
-    };
-    getData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  //     const res = await fetch(
+  //       `${import.meta.env.VITE_API_BASEURL}/score/`,
+  //       options
+  //     );
+  //     const json = await res.json();
+  //     const firstOne = json[0];
+  //     setScoreId(firstOne._id);
+  //     setData([
+  //       {
+  //         skill: "Communication Skill",
+  //         points: firstOne.communication,
+  //       },
+  //       {
+  //         skill: "Creativity",
+  //         points: firstOne.creativity,
+  //       },
+  //       {
+  //         skill: "Empathy",
+  //         points: firstOne.empathy,
+  //       },
+  //       {
+  //         skill: "Problem-Solving Skill",
+  //         points: firstOne.problemsolving,
+  //       },
+  //     ]);
+  //     setLoading(false);
+  //   };
+  //   getData();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   const elements = [
     { date: "25/05/2024", score1: 5, score2: 6, score3: 7, score4: 8 },
@@ -79,59 +79,56 @@ function Profile() {
 
   return (
     <>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <Flex
-          style={{
-            flexGrow: 1,
-          }}
-          justify={"space-between"}
-          align={"start"}
-          direction={"column"}
-        >
-          <Box>
-            <Title order={1}>ACHIVEMENT</Title>
-            <Flex align={"center"} justify={"center"} gap="xl" mt="xl">
-              <Box>
-                <Image src={mv} w={"100px"} h={"100px"} />
-              </Box>
-              <Box>
-                <Image src={mv} w={"100px"} h={"100px"} />
-              </Box>
-              <Box>
-                <Image src={mv} w={"100px"} h={"100px"} />
-              </Box>
-            </Flex>
-          </Box>
-          <Box mt="xl" w="100%">
-            <Title order={1}>HISTORY</Title>
-            <Table mt="xl">
-              <Table.Thead>
-                <Table.Tr>
-                  <Table.Th>Date taken</Table.Th>
-                  <Table.Th>Communication</Table.Th>
-                  <Table.Th>Creativity</Table.Th>
-                  <Table.Th>Empathy</Table.Th>
-                  <Table.Th>Problem Solving</Table.Th>
-                </Table.Tr>
-              </Table.Thead>
-              <Table.Tbody>{rows}</Table.Tbody>
-            </Table>
-          </Box>
+      <Flex
+        style={{
+          flexGrow: 1,
+        }}
+        justify={"space-between"}
+        align={"start"}
+        direction={"column"}
+      >
+        <Box>
+          <Title order={1}>ACHIVEMENT</Title>
+          <Flex align={"center"} justify={"center"} gap="xl" mt="xl">
+            <Box>
+              <Image src={mv} w={"100px"} h={"100px"} />
+            </Box>
+            <Box>
+              <Image src={mv} w={"100px"} h={"100px"} />
+            </Box>
+            <Box>
+              <Image src={mv} w={"100px"} h={"100px"} />
+            </Box>
+          </Flex>
+        </Box>
+        <Box mt="xl" w="100%">
+          <Title order={1}>HISTORY</Title>
+          <Table mt="xl">
+            <Table.Thead>
+              <Table.Tr>
+                <Table.Th>Date taken</Table.Th>
+                <Table.Th>Communication</Table.Th>
+                <Table.Th>Creativity</Table.Th>
+                <Table.Th>Empathy</Table.Th>
+                <Table.Th>Problem Solving</Table.Th>
+              </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>{rows}</Table.Tbody>
+          </Table>
+        </Box>
 
-          <Box ta={"center"} mx={"auto"} mt="xl">
-            <Button
-              size="lg"
-              className={styles.button}
-              component={Link}
-              to="/scenarios"
-            >
-              Take New Roleplay
-            </Button>
-          </Box>
+        <Box ta={"center"} mx={"auto"} mt="xl">
+          <Button
+            size="lg"
+            className={styles.button}
+            component={Link}
+            to="/scenarios"
+          >
+            Take New Roleplay
+          </Button>
+        </Box>
 
-          {/* <div className="chartContainer">
+        {/* <div className="chartContainer">
             <p className="chartTitle">Performance Chart</p>
             <RadarChart
               className="chart"
@@ -158,9 +155,8 @@ function Profile() {
             </div>
           </div> */}
 
-          {/* <ShareScore score={scoreId} /> */}
-        </Flex>
-      )}
+        {/* <ShareScore score={scoreId} /> */}
+      </Flex>
     </>
   );
 }
