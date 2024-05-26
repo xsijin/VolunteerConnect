3a341d0dc8d2d39b64478a7b515e897dee1047b4
mintaekim <kmintae33@gmail.com>
2024-05-26 13:58:42 +0800

    return data

--- backend/controllers/surveyQns.js
+++ backend/controllers/surveyQns.js
@@ -8,6 +8,7 @@ async function createSurvey(req, res) {
   console.log("createSurvey");
   try {
     const data = await modelSurvey.createSurvey(req.body);
+    res.json(data);
   } catch (err) {
     console.error(err);
     res.status(500).json({ errorMsg: err.message });
